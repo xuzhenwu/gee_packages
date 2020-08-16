16dab48b370d11c236817da72cf4375f0f844baa
kongdd.sysu <kongdd.sysu@gmail.com>
2020-08-16 11:39:27 +0000

    Updated tests/test-buffer_clipImgCol.js (added 4 lines, deleted 2 lines).

--- tests/test-buffer_clipImgCol.js
+++ tests/test-buffer_clipImgCol.js
@@ -2,7 +2,8 @@
 var imgcol_terraClimate = ee.ImageCollection("IDAHO_EPSCOR/TERRACLIMATE"),
     imageCollection2 = ee.ImageCollection("NASA/ORNL/DAYMET_V3"),
     imageCollection3 = ee.ImageCollection("WORLDCLIM/V1/MONTHLY"),
-    st_212 = ee.FeatureCollection("projects/pml_evapotranspiration/shp/phenofit/st212");
+    st_212 = ee.FeatureCollection("projects/pml_evapotranspiration/shp/phenofit/st212"),
+    imgcol_MOD15A2 = ee.ImageCollection("MODIS/006/MOD15A2H");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 // print(imgcol_terraClimate.limit(3));
 var pkg_buffer = require('users/kongdd/public:pkg_buffer.js');
@@ -15,4 +16,5 @@ var options = {
     folder   : '', // drive forder
     fileFormat: 'csv'      // 'csv' or 'geojson'
 };
-pkg_buffer.spClipImgCol(imgcol_terraClimate, st_212, "terraClimate", options);
+// pkg_buffer.spClipImgCol(imgcol_terraClimate, st_212, "terraClimate", options);
+pkg_buffer.spClipImgCol(imgcol_MOD15A2, st_212, "flux212_MOD15A2", options);
